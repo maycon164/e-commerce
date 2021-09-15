47304cf8643d38f849ffc056d3ebe5292814e599
maycon164 <mayconfelipe164@gmail.com>
2021-09-15 10:27:46 -0300

    product scheme

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,12 +1,74 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
+
+    name:{
+        type: String,
+        required: true
+    },
+    
+    description:{
+        type : String,
+        required: true
+    },
+
+    richDescription:{
+        type: String,
+        default: ''
+    },
+    
+    image:{
+        type: String,
+        required: true
+    },
+
+    images: [{
+        type: String
+    }],
+    
+    brand:{
+        type: String,
+        default: ''
+    },
+    
+    price: {
         type: Number,
         required: true
+    },
+
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
+
+    countInStock: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 200
+    },
+
+    rating: {
+        type: Number,
+        default: 0
+    },
+
+    numReviews:{
+        type:Number,
+        default: 0
+    },
+
+    isFeatured: {
+        type: Boolean,
+        default: false
+    },
+
+    dateCreated: {
+        type : Date,
+        default: Date.now()
     }
+
 })
 
 exports.Product = mongoose.model('Products', productSchema);
