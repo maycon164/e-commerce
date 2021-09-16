032246915c1f3f4afefb98f9819f8a814d408f3f
maycon164 <mayconfelipe164@gmail.com>
2021-09-16 13:10:44 -0300

    scheme product id virtual

--- backend/models/product.js
+++ backend/models/product.js
@@ -71,4 +71,12 @@ const productSchema = mongoose.Schema({
 
 })
 
+productSchema.virtual('id').get(function(){
+    return this._id.toHexString();
+});
+
+productSchema.set('toJSON',{
+    virtuals: true,
+});
+
 exports.Product = mongoose.model('Products', productSchema);
