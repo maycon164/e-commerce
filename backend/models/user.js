cd2233a3022495455c783f07ea4d79179aeaa137
maycon164 <mayconfelipe164@gmail.com>
2021-09-16 20:02:12 -0300

    user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,15 +1,66 @@
 const mongoose = require('mongoose');
 
-
 const userSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    
+    phone: {
         type: Number,
         required: true
+    },
+    
+    country: {
+        type: String,
+        default: ''
+    },
+    
+    city: {
+        type: String,
+        default: ''
+    },
+    
+    zip: {
+        type: String,
+        default: ''
+    },
+            
+    street: {
+        type: String,
+        defult: ''
+    },
+
+    apartment: {
+        type: String,
+        default: ''
+    },
+    
+    isAdmin: {
+        type: Boolean,
+        default: false
     }
+
+});
+
+userSchema.virtual('id').get(function(){
+    return this._id.toHexString();
 })
 
-
+userSchema.set('toJSON', {
+    virtuals: true
+});
 
 exports.User = mongoose.model('User', userSchema);
+exports.userSchema = userSchema;
