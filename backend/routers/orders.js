acb8b5b664b45c1e2ebba2ecdcd3fae6c16e6b3c
maycon164 <mayconfelipe164@gmail.com>
2021-09-19 14:46:29 -0300

    order, update status and delete order

--- backend/routers/orders.js
+++ backend/routers/orders.js
@@ -9,6 +9,7 @@ module.exports = (app) => {
 
     const api = process.env.API_URL;
 
+    //LISTAR TODOS OS PEDIDOS
     app.get(`${api}/order`, async (req, res) => {
 
         const orderList = await Order.find().populate('user', 'name').sort('dateOrdered');
@@ -20,7 +21,7 @@ module.exports = (app) => {
    
     });
 
-
+    //find by id
     app.get(`${api}/order/:id`, async (req, res) => {
         let order = await Order.findById(req.params.id)
         .populate('user', 'name')
@@ -43,6 +44,7 @@ module.exports = (app) => {
         return res.status(200).send(order);
     });
 
+    //Cadastrar novo pedido
     app.post(`${api}/order`, async (req, res) => {
 
         let orderItensIds = Promise.all (req.body.orderItems.map(async orderItem => {
@@ -81,4 +83,44 @@ module.exports = (app) => {
         return res.status(200).send(order);
 
     });
+
+    //ATUALIZAR STATUS DO PEDIDO
+    app.put(`${api}/order/:id`, async (req, res) => {
+        
+        let order = await Order.findByIdAndUpdate(req.params.id, {
+            status: req.body.status
+        },{
+            new: true,
+        });
+
+        if(!order)
+            return res.status(400).send("it cannot update the order");
+
+        return res.status(200).send(order);
+    });
+
+    //DELETE ORDER
+
+    app.delete(`${api}/order/:id`, async (req, res) => {
+        
+        Order.findByIdAndRemove(req.params.id).then(order => {
+            if(order){
+                
+                order.orderItems.forEach(async item => {
+                    await OrderItem.findByIdAndRemove(item._id)
+                });
+
+                return res.status(200).send(order);
+
+            }else{
+
+                return res.status(404).send("cannot remove order");
+            
+            }
+        }).catch(err => {
+            return res.status(500).json({message: err});
+        });
+
+    });
+
 }
